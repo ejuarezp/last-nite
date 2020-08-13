31976554b80a32175687a2a888d4191810f1d229
Eduardo Juárez <edjuarezp@tec.mx>
2020-08-13 16:36:30 +0000

    Sesión 2, ejemplso básicos de C

--- main.cpp
+++ main.cpp
@@ -1,3 +1,31 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+
+using namespace std; //Para no tener que escribir siempre std::
+
+void incrementa(int *variable) {
+  (*variable)++;
+}
+
 int main() {
+  //Ya junciona :3
+  int numero = 5;
+  incrementa(&numero);
+  cout << numero << endl;
+
+  //printf("Hola mundo!\n");
+  cout << "Hola mundo!\n" << "Cuantos alumnos son: "; //cout: console out
+  int alumnos = 0;
+  
+  //scanf("%i%*c", &alumnos);
+  cin >> alumnos;//cin: console in
+
+  cout << "Somos " << alumnos << "\n";
+  float promedio = 95.5;
+  cout << promedio << endl; //endl: end line
+
+  //cerr: console error
+
   return 0;
 }
